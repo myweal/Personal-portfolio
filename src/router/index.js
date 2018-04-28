0b708cd83884f0ba7bf477f9557b1e44d2dbc1bb
liushanshan <liushanshan@csii.com.cn>
2018-04-28 17:01:05 +0800

    change

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,10 @@ Vue.use(VueMeta)
 export default new Router({
   routes: [
     {
+      path: '',
+      redirect: '/login'
+    },
+    {
       path: '/login',
       name: 'login',
       component: login
--- src/views/Login/login.vue
+++ src/views/Login/login.vue
@@ -2,7 +2,7 @@
   <div>
     <Header hideFlag="true" title="登录"></Header>
     <main class="container">
-      <form class="form-horizontal" name="loginForm" novalidate @submit.prevent="subForm()">
+      <form class="form" name="loginForm" novalidate @submit.prevent="subForm()">
         <div class="form-group">
           <label for="username">用户名</label>
           <input id="username" type="text" class="form-control form-control-sm" v-model="user.username"/>
@@ -11,9 +11,11 @@
           <label for="psdId">密码</label>
           <input id="psdId" type="text" class="form-control form-control-sm" v-model="user.psd"/>
         </div>
-        <button>
-          登录
-        </button>
+        <div class="text-center">
+          <button type="submit" class="btn col-6 btn-primary">
+            登录
+          </button>
+        </div>
       </form>
     </main>
   </div>
